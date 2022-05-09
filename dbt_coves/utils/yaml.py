a4461b2e0b5102be41d70053f67b20d184dbc6d5
z3z1ma <butler.alex2010@gmail.com>
2022-05-08 21:03:23 -0700

    initial bits of refactoring, much more to do

--- dbt_coves/utils/yaml.py
+++ dbt_coves/utils/yaml.py
@@ -1,17 +1,17 @@
 """Contains yaml related utils which might get used in places."""
 
-import collections
 from pathlib import Path
 from typing import Any, Dict
+from functools import lru_cache, wraps
 
-import yaml
-import yamlloader
+from ruamel.yaml import YAML
 
 from dbt_coves.core.exceptions import YAMLFileEmptyError
 
 
+@lru_cache(maxsize=1)
 def open_yaml(path: Path) -> Dict[str, Any]:
-    """Opens a yaml file... Nothing too exciting there.
+    """Open a YAML file.
 
     Args:
         path (Path): Full filename path pointing to the yaml file we want to open.
@@ -20,21 +20,72 @@ def open_yaml(path: Path) -> Dict[str, Any]:
         Dict[str, Any]: A python dict containing the content from the yaml file.
     """
     if path.is_file():
-        with open(path, "r") as stream:
-            yaml_dict = yaml.load(stream, Loader=yamlloader.ordereddict.CSafeLoader)
-            if yaml_dict:
-                return yaml_dict
-            raise YAMLFileEmptyError(f"The following file {path.resolve()} seems empty.")
+        contents = YAML().load(path)
+        if contents:
+            return contents
+        raise YAMLFileEmptyError(f"The following file {path.resolve()} seems empty.")
     raise FileNotFoundError(f"File {path.resolve()} was not found.")
 
 
 def save_yaml(path: Path, data: Dict[str, Any]) -> None:
-    """Saves a YAML content.
+    """Serializes a json-like object to a yaml string and writes to file.
 
     Args:
         path (Path): Full filename path pointing to the yaml file we want to save.
         data (dict[str, Any]): Data to save in the file.
     """
-    with open(path, "w") as outfile:
-        data_order_dict = collections.OrderedDict(data)
-        yaml.dump(data_order_dict, outfile, width=100, Dumper=yamlloader.ordereddict.CDumper)
+    YAML().dump(data, path)
+
+
+class YamlHandler:
+    def __init__(self, path: Path):
+        self._path = path
+        self.config = open_yaml(path)
+
+    @property
+    def path(self):
+        return self._path
+
+    def save(self) -> None:
+        """Save the YAML file handled by this class"""
+        save_yaml(self.path)
+
+    def getter(self, value: str):
+        """Use this to wrap a function turning it into a value getter.
+
+        Example:
+
+            @y.getter("config.tool.name")
+            def config_name():  pass
+
+        """
+
+        @wraps
+        def getter_func(f):
+            keys = value.split(".")
+            parsed_value = None
+            for key in keys:
+                parsed_value = (
+                    self.config[key] if parsed_value is None else parsed_value[key]
+                )
+            return parsed_value
+
+        return getter_func
+
+    def __getitem__(self, key):
+        return self.config[key]
+
+    def __setitem__(self, key, value):
+        self.config[key] = value
+
+    def __delitem__(self, key):
+        del self.config[key]
+
+    def __contains__(self, key):
+        return key in self.config
+
+    def __len__(self):
+        return len(self.config)
+
+    def __repr__(self):
+        return repr(self.config)
